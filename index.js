0e409510248d0fe9d15ddb1870e7efcf16aa35fa
Esau890 <essa_u95@hotmail.com>
2023-10-14 03:00:17 -0400

    Update

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
-
+const Product = require('./models/products'); 
 const routes = require('./routes/routes');
-
-const app = express(); // Define the express app first
+const app = express(); 
 
 app.use(express.json());
 app.use('/api', routes);
@@ -15,18 +13,17 @@ app.get('/', (req, res) => {
   res.send('Welcome to the main page of your API!');
 });
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'Marketplace' });
+        console.log('MongoDB Connected to Marketplace Database');
 
-database.on('error', (error) => {
-    console.log(error);
-});
+        app.listen(3000, () => {
+            console.log(`Server Started at ${3000}`);
+        });
+    } catch (error) {
+        console.error('MongoDB Connection Error:', error);
+    }
+};
 
-database.once('connected', () => {
-    console.log('Database Connected');
-
-    // Move the app.listen here
-    app.listen(3000, () => {
-        console.log(`Server Started at ${3000}`);
-    });
-});
+connectToDatabase();
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router()
+const product = require('../models/products'); 
 const { MongoClient, ObjectID } = require('mongodb');
 
 
 router.get('/products', async (req, res) => {
-    res.send('Get all products');
+    try {
+        const data = await product.find()
+        res.json(data)
+    } catch (err) {
+        res.json({ message: err })
+    }
   });
   
   router.get('/products/:id', async (req, res) => {
@@ -12,8 +18,24 @@ router.get('/products', async (req, res) => {
   });
   
   router.post('/products', async (req, res) => {
-    res.send('Add new product');
-  });
+    const data = new product({
+        name: req.body.name,
+        category: req.body.category,
+        quantity: req.body.quantity,
+        price: req.body.price,
+        description: req.body.description
+    });
+
+    try {
+        const savedData = await data.save();
+        res.status(201).json(savedData);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error saving to the database' });
+    
+    }
+});
+
   
   router.put('/products/:id', async (req, res) => {
     res.send('Update product by ID');
@@ -26,9 +48,9 @@ router.get('/products', async (req, res) => {
   router.delete('/products', async (req, res) => {
     res.send('Delete all products');
   });
-  
-  router.get('/products/search/:name', async (req, res) => {
+  router.get('/products', async (req, res) => {
     res.send('Find products by name');
+    
   });
 
 module.exports = router;
